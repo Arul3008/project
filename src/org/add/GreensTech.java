3c58bda19876f3ceb94b9d2796fd8dff33b06c69
Arul3008 <arulk3777@gmail.com>
2023-02-21 23:15:30 -0800

    new code added

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,38 @@
+package org.add;
+
+public class GreensTech {
+
+	private void greensOmr() {
+		// TODO Auto-generated method stub
+		System.out.println("merina bachi");
+		
+	}
+	private void greensAdayar (){
+		// TODO Auto-generated method stub
+		System.out.println("frist street");
+		
+	}
+	private void greensTambaram() {
+		// TODO Auto-generated method stub
+		System.out.println("2 nagar kovil ");
+		
+	}
+	private void greensvelacherry() {
+		// TODO Auto-generated method stub
+		System.out.println("3 thiruvanmair");
+		
+	}
+	private void greensAnnanager() {
+		// TODO Auto-generated method stub
+		System.out.println("namma chenni");
+	}
+	public static void main(String[] args) {
+		GreensTech green = new GreensTech();
+		green.greensOmr();
+		green.greensAdayar();
+		green.greensTambaram();
+		green.greensvelacherry();
+		green.greensAnnanager();
+	}
+}
+
